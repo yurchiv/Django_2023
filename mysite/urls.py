b703f11a9f543dc4427ec9136a41cc1982d598be
yurchiv <yurchiv@ukr.net>
2023-09-07 20:41:11 +0300

    #LAB_2

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'', include('app_blog.urls')),
 ]
